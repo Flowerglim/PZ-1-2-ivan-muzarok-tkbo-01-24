a03ac7bde93803788099cb2f5c925092bb921353
Flowerglim <ivanmuzarok@gmail.com>
2025-11-07 16:50:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,8 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const forms = document.querySelectorAll('.needs-validation');
-
-//   forms.forEach(form => {
-//     const alert = form.querySelector('#myAlert1');
-//     const modalContent = form.closest('.modal-content');
-
-//     form.addEventListener('submit', event => {
-//       event.preventDefault();
-//       event.stopPropagation();
-
-//       if (form.checkValidity()) {
-//         alert.style.display = 'block';
-//         alert.classList.add('show');
-
-//         modalContent.style.paddingBottom = '2rem';
-
-//         setTimeout(() => {
-//           alert.classList.remove('show');
-//           setTimeout(() => {
-//             alert.style.display = 'none';
-//             modalContent.style.paddingBottom = '';
-//           }, 300); 
-//         }, 3000);
-//       }
-
-//       form.classList.add('was-validated');
-//     });
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
   const forms = document.querySelectorAll('.needs-validation');
 
   forms.forEach(form => {
-    const alert = form.querySelector('.alert'); 
+    const alert = form.querySelector('.alert');
 
     form.addEventListener('submit', event => {
       event.preventDefault();
@@ -60,12 +29,9 @@ document.addEventListener('DOMContentLoaded', () => {
           }, 3000);
         }
 
-        // form.reset();
-        // form.classList.remove('was-validated');
       }
 
       form.classList.add('was-validated');
     });
   });
 });
-
